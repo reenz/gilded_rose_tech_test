f34a553182777f128f39679baf48541f4976fd7e
reena <reena.sanjay.sharma@gmail.com>
2018-02-28 13:11:42 +0000

    test for backstage passes where sellin is 10

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,17 +3,32 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    it "reduces the quality by 1 for normal item" do
-      items = [Item.new("Item", 5, 5)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 4
+    describe "normal items" do
+      it "reduces the quality by 1 " do
+        items = [Item.new("Item", 5, 5)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 4
+      end
+
+      it "decrease the quality by 2 if sellIn is < 0" do
+        items = [Item.new("Item", 0, 5)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 3
+      end
+
+      it "reduces the sellIn by 1" do
+        items = [Item.new("Item", 5, 5)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 4
+      end
     end
 
-    it "reduces the sellIn by 1 for normal item" do
-      items = [Item.new("Item", 5, 5)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq 4
+    describe "Backstage passes" do
+      it "value increases by 2 times if sellIn is <= 10" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10,8)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 10
+      end
     end
   end
-
 end
